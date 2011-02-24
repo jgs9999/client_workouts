f304485f179e771554fda7def77d9e08138d02b3
jeff <jeffshea206@gmail.com>
2011-02-23 22:02:16 -0800

    HF Rails chap4 pg177

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ClientWorkouts::Application.config.secret_token = 'c180e827ba894b56a82d3813e1f1e82e7ca107c45489d7b2c9a450b559306034f167cadee0df78fea8a85aff1cb21fa6d85bbe7564d59b1baf712d78197b3b0f'
